576d2a10d3aa11e1fee8cb8f4a22e8ba55e6760c
XpeditionAI <root1@hydras-MacBook-Air.local>
2025-09-07 23:25:48 +0400

    Allow Next/Image domain + TS/ESLint fixes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   outputFileTracingRoot: __dirname,
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
+  },
+  // keep this true if you still hit lint errors during build
   eslint: { ignoreDuringBuilds: true },
 };
