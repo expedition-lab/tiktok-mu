329f89504e277efa3f38477a3841ed4589d536e0
XpeditionAI <root1@hydras-MacBook-Air.local>
2025-09-08 02:22:18 +0400

    Fix not-found page: correct JSK and use next/link

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ module.exports = {
   outputFileTracingRoot: __dirname,
   images: { remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }] },
   // keep this true until everything is green in prod, then flip to false
-  eslint: { ignoreDuringBuilds: true },
+  eslint: { ignoreDuringBuilds: false },
 };
